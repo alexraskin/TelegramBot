92844bf3298eada601ddd738c4ed8b72b50eb964
Alex <37590597+alexraskin@users.noreply.github.com>
2021-11-27 00:42:07 +0000

    updates

--- main.py
+++ main.py
@@ -4,18 +4,10 @@ import random
 
 import aiofiles
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
 
 from reinhardt import ReinhardtBot
-from utils import (
-  about_text,
-  shield,
-  charge,
-  help_text,
-  rocket_hammer,
-  fire_strike,
-  earth_shatter,
-)
+from utils import (about_text, charge, earth_shatter, fire_strike, help_text,
+                   rocket_hammer, shield)
 
 API_TOKEN = os.environ['API_KEY']
 REIN_URL = 'https://overwatch.fandom.com/wiki/Reinhardt'
@@ -30,24 +22,6 @@ logging.basicConfig(
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
-
-
-
-keyboard1 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add("Doggo? 🐕").add("Meme? 🐸")
-
-
-@dp.message_handler(commands=['funny'])
-async def welcome(message: types.Message):
-    await message.reply('Pick One?', reply_markup=keyboard1)
-
-@dp.message_handler()
-async def kb_answer(message: types.Message):
-    if message.text == 'Doggo? 🐕':
-        await ReinhardtBot.send_photo(bot, message.chat.id, dog=True)
-    elif message.text == 'Meme? 🐸':
-        await ReinhardtBot.send_photo(bot, message.chat.id, meme=True)
-    else:
-        await message.reply(f"Your message is: {message.text}")
 
 
 @dp.message_handler(commands=['start'])
